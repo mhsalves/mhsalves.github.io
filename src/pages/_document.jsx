7fd92f7f3ca15641a811b5b9404fc676826281a7
Matheus Alves <matheus.alves@meliuz.com.br>
2019-10-23 04:46:51 -0400

    update: server style sheet

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class extends Document {
   static async getInitialProps(ctx) {
+    const { renderPage } = ctx;
+    const sheet = new ServerStyleSheet();
+    const page = renderPage((App) => (props) => sheet.collectStyles(<App {...props} />));
+    const styleTags = sheet.getStyleElement();
     const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+    return { ...initialProps, ...page, styleTags };
   }
 
   render() {
@@ -12,6 +17,7 @@ export default class extends Document {
       <html lang="br">
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          {this.props.styleTags}
         </Head>
         <body>
           <Main />
