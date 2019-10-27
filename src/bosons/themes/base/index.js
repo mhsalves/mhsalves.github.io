189eabe6e7ddb02679bb542a0797f7bf48d6b40c
Matheus Alves <matheus.alves@meliuz.com.br>
2019-10-27 01:49:49 -0400

    theme: widths

--- src/bosons/themes/base/index.js
+++ src/bosons/themes/base/index.js
@@ -1,5 +1,7 @@
 import breakpoints from './breakpoints';
+import widths from './widths';
 
 export default {
   breakpoints,
+  widths,
 };
---
+++ src/bosons/themes/base/widths.js
@@ -0,0 +1,5 @@
+const widths = {
+  max: '1152px',
+};
+
+export default widths;
