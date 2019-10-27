39c649ae207f9961c3b7efac1ab71903e7cc1658
Matheus Alves <matheus.alves@meliuz.com.br>
2019-10-27 01:23:29 -0400

    example: with breakpoints

--- src/atoms/Example/styles.js
+++ src/atoms/Example/styles.js
@@ -4,6 +4,10 @@ const Text = styled.p`
   font-size: 1.333rem;
   line-height: 1.5rem;
   margin: 0;
+
+  ${({ theme: { breakpoints } }) => breakpoints.sm} {
+    margin: 24px;
+  }
 `;
 
 export default {
