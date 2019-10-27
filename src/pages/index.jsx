21cbd3b9eb8889186cc6dbcc051c2097834f1bbc
Matheus Alves <matheus.alves@meliuz.com.br>
2019-10-26 22:27:12 -0400

    example: styled component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
+import Example from '../atoms/Example';
+
 function Home() {
   return (
     <div>
+      <Example />
       <h1>Hello World ! (Home)</h1>
       <a href="about">Ir para About</a>
     </div>
