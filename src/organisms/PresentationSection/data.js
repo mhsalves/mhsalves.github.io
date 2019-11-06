99f8ec5faf06bb782e10d982b2b61cd32d48ed0e
Matheus Alves <matheus.alves@meliuz.com.br>
2019-11-06 05:36:05 -0400

    feat: PresentationSection

---
+++ src/organisms/PresentationSection/data.js
@@ -0,0 +1,11 @@
+const prefixTitle = 'Hello, I\'m ';
+const myName = 'Matheus Alves'.replace(/ ([^ ]*)$/, '\u00a0$1');
+const description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque placerat nulla at sapien dictum, gravida fringilla risus commodo. Quisque velit elit, vestibulum sit amet efficitur at, fringilla a justo.';
+const moreButton = 'About me';
+
+export default {
+  prefixTitle,
+  myName,
+  description,
+  moreButton,
+};
