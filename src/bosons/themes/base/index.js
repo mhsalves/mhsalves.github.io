ec02519363207843a9212bd6f7066ddf1da04717
Matheus Alves <matheus.alves@meliuz.com.br>
2019-11-06 04:37:25 -0400

    theme: colors

--- src/bosons/themes/base/index.js
+++ src/bosons/themes/base/index.js
@@ -1,10 +1,12 @@
 import breakpoints from './breakpoints';
+import colors from './colors';
 import typography from './typography';
 import zIndex from './zIndex';
 import widths from './widths';
 
 export default {
   breakpoints,
+  colors,
   typography,
   zIndex,
   widths,
