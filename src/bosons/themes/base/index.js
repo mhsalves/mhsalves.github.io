73c1aec4ebbcae95e12868406a4e169a535503b4
Matheus Alves <matheus.alves@meliuz.com.br>
2019-11-06 04:37:07 -0400

    theme: init zIndex

--- src/bosons/themes/base/index.js
+++ src/bosons/themes/base/index.js
@@ -1,9 +1,11 @@
 import breakpoints from './breakpoints';
 import typography from './typography';
+import zIndex from './zIndex';
 import widths from './widths';
 
 export default {
   breakpoints,
   typography,
+  zIndex,
   widths,
 };
