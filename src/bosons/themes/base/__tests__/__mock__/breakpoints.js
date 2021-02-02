28686dedf9206b6db2c517e7393467557dfc980e
Matheus Alves <matheus.alves@meliuz.com.br>
2021-02-01 22:35:44 -0400

    test: add theme breakpoint

---
+++ src/bosons/themes/base/__tests__/__mock__/breakpoints.js
@@ -0,0 +1,14 @@
+const labels = ['xs', 'sm', 'md', 'lg', 'xl'];
+
+const data = {
+  xs: '@media (min-width: 0px)',
+  sm: '@media (min-width: 576px)',
+  md: '@media (min-width: 768px)',
+  lg: '@media (min-width: 992px)',
+  xl: '@media (min-width: 1200px)',
+};
+
+export default {
+  labels,
+  data,
+};
