04678108d908195488f4c25aeb264f83c8a341a6
Matheus Alves <matheus.alves@meliuz.com.br>
2021-02-02 17:57:51 -0400

    update: add Basic MetaTags in index page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 
 import PresentationSection from '../sections/PresentationSection';
 
+import BasicMetaTags from '../bosons/MetaTags/Basic';
+
 function Home() {
   return (
     <>
+      <BasicMetaTags />
       <PresentationSection />
     </>
   );
