9e2a9a42de064ac3fec6babc4e72513a8cc2034f
Matheus Alves <30807170+mhsalves@users.noreply.github.com>
2026-09-09 10:07:49 -0400

    release: new features

---
+++ docs/_next/static/FeF160ZGJFXD-oqKqezrg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-65b2661f540ce4ed.js"],"/_error":["static/chunks/pages/_error-f7153c1399372827.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
